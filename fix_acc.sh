41a27c7ec47a8383d3662d36eeb5e446038aab9b
thanhdung96 <14dh110224@st.huflit.edu.vn>
2021-05-20 15:42:13 +0000

    Finished first release of fix aac script
    Modifications qui seront validées :
    	modifié :         fix_acc.sh

--- fix_acc.sh
+++ fix_acc.sh
@@ -3,6 +3,7 @@
 # script used for fixing AAC error while downloading videos using youtube-dl
 # the input can be a directory, an mp4 file or multiple mp4 files.
 
+# if there is only one parameter
 if [ $# -eq 1 ]; then
 	if [ -d $1 ]; then
 		# if it is a directory and it is not empty
@@ -15,17 +16,23 @@ if [ $# -eq 1 ]; then
 		else	# if it is empty
 			echo "empty directory"
 		fi
-	else
-		echo "this is not a directory"
+	elif [ -f $1 ]; then
+		echo "Processing $1..."
+		ffmpeg -i $1 -c copy -bsf:a aac_adtstoasc output_$1
 	fi
+# if there are more than one parameter
 elif [ $# -gt 1 ]; then
-	# if there is only one parameter
 	# and it does exist and is a regular file
-	if [ -f $1 ]; then
-		ffmpeg -i $1 -c copy -bsf:a aac_adtstoasc output_$1.mp4
-	fi
+	for file in $@
+	do
+		if [ -f $file ]; then
+			echo "Processing $file..."
+			ffmpeg -i $file -c copy -bsf:a aac_adtstoasc output_$file
+		elif [ -d file ]; then
+			echo "$file is a directory."
+		fi
+	done
 elif [ $# -eq 0 ]; then
 	echo "there are no argument."
 fi
 
-#ffmpeg -i 62303237_p3.mp4 -c copy -bsf:a aac_adtstoasc output_p3.mp4
