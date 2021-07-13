9cb6acb4950075e206646a015212b1a9beafc953
thanhdung96 <14dh110224@st.huflit.edu.vn>
2021-07-13 23:40:16 +0700

    Added check for cifs-utils before trying to connect to server.
    Modifications qui seront validées :
    	modifié :         connect_smb.sh

--- connect_smb.sh
+++ connect_smb.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# check if cifs-utils is installed
+dpkg -s cifs-utils &>/dev/null || { 
+	echo "cifs-utils is not installed.\n";
+	echo "please install the package using:";
+	echo "sudo apt-get update";
+	echo "sudo apt-get install cifs-utils";
+	exit;
+}
+
 sudo mount -t cifs -o username=thanhdung96,vers=1.0 //192.168.1.132/thanhdung96 /mnt/cifs
