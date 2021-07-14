e04af102fe4bbb4c36a90c7d599b72551f91494f
thanhdung96 <14dh110224@st.huflit.edu.vn>
2021-07-14 23:28:58 +0700

    Added merging for file type.
    Modifications qui seront validées :
    	modifié :         merge_av.sh

--- merge_av.sh
+++ merge_av.sh
@@ -17,6 +17,18 @@ numberOfVids=0
 numberOfAudio=0
 MAX_ARG=4
 
+# actual merging function
+# first argument is audio input
+# second argument is video output
+# third argument is video output
+function mergeAudioVideo() {
+	absolutePath=$(realpath $3)
+	vidFileName=$(basename $2)
+
+	ffmpeg -i $2 -i $1 -c copy $absolutePath/output_$vidFileName
+	echo "processed: output_"$vidFileName
+}
+
 # check if there are enough arguments
 if [ $# -lt $MAX_ARG ]; then
 	echo "not enough arguments."
@@ -29,9 +41,9 @@ elif [ $# -gt $MAX_ARG ]; then
 fi
 
 # check if ffmpeg exists in the machine
-type -P ffmpeg &>/dev/null && continue || {
-	echo "ffmpeg command not found."
-	exit 1
+type -P ffmpeg &>/dev/null || {
+	echo "ffmpeg command not found.";
+	exit 1;
 }
 
 # update field separator to new line to process files
@@ -44,11 +56,13 @@ if [ $1 == "-f" ] || [ $1 == "--file" ]; then
 	if [ ! -f $2 ] || [ ! -f $3 ]; then
 		echo "the input must be files."
 	else
+		mergeAudioVideo $2 $3 $4
 	fi
 elif [ $1 == "-d" ] || [ $1 == "--dir" ]; then
 	# check if input is a directory
 	if [ ! -d $2 ] || [ ! -d $3 ]; then
 		echo "the input must be directories."
 	else
+		echo "empty"
 	fi
 fi
